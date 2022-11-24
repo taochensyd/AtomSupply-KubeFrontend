7c43593620d29ddf637449ad4d3195ce0cd90efd
Tao Chen <taochensyd@gmail.com>
2022-11-25 08:10:27 +1100

    add fetch api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
-import './App.css';
+import "./App.css";
 
 function App() {
+  const makePostUpdateRequest = (body) => {
+    fetch("https://kube-api-endpoint.atom.com.au/update", {
+      Method: "POST",
+      Headers: {
+        Accept: "application.json",
+        "Content-Type": "application/json",
+      },
+      Body: body,
+      Cache: "default",
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        return data;
+      });
+  }
+
   return (
     <div className="App">
-      
+      `fgfd$
+      {console.log(
+        makePostUpdateRequest()
+      )}
+      `
     </div>
   );
 }
