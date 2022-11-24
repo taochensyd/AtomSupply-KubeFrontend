a4bd12264cac9aa8564d354a9f507a19ba1484f4
Tao Chen <taochensyd@gmail.com>
2022-11-24 15:58:47 +1100

    add container update in componemnts folder

---
+++ src/componments/container.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+export const container = () => {
+  async function postData(url = "", data = {}) {
+    const data = {
+      token: "2sGMxTwKeClnILXa3aK2",
+      environment: "uat",
+      image: "atomportal_frontend",
+      tag: "develop",
+    };
+
+    fetch("https://example.com/profile", {
+      method: "POST", // or 'PUT'
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Success:", data);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+    s;
+  }
+
+  return <div>container</div>;
+};
